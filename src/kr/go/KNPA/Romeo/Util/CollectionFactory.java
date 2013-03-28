7129052a99e73478fb8e64bd4fa00d98126c791e
Young-Woo Choe <projet6634@gmail.com>
2013-03-28 10:54:52 +0900

    make survey event a subevent of meesage

--- src/kr/go/KNPA/Romeo/Util/CollectionFactory.java
+++ src/kr/go/KNPA/Romeo/Util/CollectionFactory.java
@@ -3,17 +3,30 @@ package kr.go.KNPA.Romeo.Util;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+
+/**
+ * Helps create Java Collection objects easily  
+ * @author Chae Hosik
+ */
 public class CollectionFactory {
 
 	public CollectionFactory() {
 	}
 	
+	/**
+	 * 여러 개의 (string,object) 페어를 입력하여 
+	 * HashMap <string,object> 객체를 생성한다.
+	 * @param ...objects string, object, string, objects 순으로 pair를 입력     
+	 * @return hashmap
+	 * 
+	 */
 	public static HashMap<String, Object> hashMapWithKeysAndValues(Object ...objects) {
 		HashMap<String, Object> hashMap = new HashMap<String, Object>();
 		if(objects.length % 2 != 0) {
 			RuntimeException e = new RuntimeException("objects length must be even");
 			throw e;
 		}
+		
 		for(int i=0; i<objects.length; i+=2) {
 			String key = (String)objects[i];
 			Object value = (Object)objects[i+1];
