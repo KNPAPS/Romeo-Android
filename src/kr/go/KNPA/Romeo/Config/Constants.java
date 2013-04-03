6c5a042fd269b51dfba6953590ea2362595f1cca
Young-Woo Choe <projet6634@gmail.com>
2013-04-03 11:20:29 +0900

    added new connection, config package

---
+++ src/kr/go/KNPA/Romeo/Config/Constants.java
@@ -0,0 +1,14 @@
+package kr.go.KNPA.Romeo.Config;
+
+/**
+ * 일반적으로 사용되는 공통된 상수들의 모음
+ * @author 최영우
+ * @since 2013.04.02
+ */
+public class Constants {
+	//! Type이나 기타 int형 변수의 기본 설정값
+	public static final int NOT_SPECIFIED = -777;
+	//! 경찰 계급 순서
+	public static final String[] POLICE_RANK = 
+			{"치안총감", "치안정감", "치안감", "경무관", "총경", "경정", "경감", "경위", "경사", "경장", "순경", "의경"};
+}
--- src/kr/go/KNPA/Romeo/Util/ImageManager.java
+++ src/kr/go/KNPA/Romeo/Util/ImageManager.java
@@ -15,9 +15,21 @@ import android.net.Uri;
 import android.provider.MediaStore.Images;
 import android.util.Log;
 
+/**
+ *  이미지 파일 관리\n
+ *  uri에서 이미지 가져오거나, 로컬 파일 시스템에서 이미지파일을 삭제하거나 로드\n
+ *  또는 파일을 byte array로 또는 그 반대로 변환한다
+ */
 public class ImageManager {
 	
-	// 전체 Media Scanning하지 않고 특정 파일만 스캐닝 하기 :: http://www.androidpub.com/1953144
+
+	/**
+	 * 비트맵 파일을 byte array로 바꾼다
+	 * 전체 Media Scanning하지 않고 특정 파일만 스캐닝함
+	 * @see http://www.androidpub.com/1953144
+	 * @param $bitmap
+	 * @return byteArray
+	 */
 	public static byte[] bitmapToByteArray( Bitmap $bitmap ) {  
         ByteArrayOutputStream stream = new ByteArrayOutputStream() ;  
         $bitmap.compress( CompressFormat.JPEG, 50, stream) ;  
@@ -25,11 +37,22 @@ public class ImageManager {
         return byteArray ;  
     }  
 	
+	/**
+	 * byte array를 bitmap 파일로 변환
+	 * @param $byteArray
+	 * @return bitmap
+	 */
 	public static Bitmap byteArrayToBitmap( byte[] $byteArray ) {  
 	    Bitmap bitmap = BitmapFactory.decodeByteArray( $byteArray, 0, $byteArray.length ) ;  
 	    return bitmap ;  
 	} 
 	
+	/**
+	 * uri로부터 비트맵 파일 가져오기
+	 * @param context
+	 * @param uri
+	 * @return bitmap
+	 */
 	public static Bitmap bitmapFromURI (Context context, Uri uri) {
 		Bitmap b = null;
 		try {
@@ -47,18 +70,47 @@ public class ImageManager {
 		return b;
 	}
 	
+	/**
+	 * uri에서 bitmap을 가져와서 path로 저장
+	 * @param context
+	 * @param picURI
+	 * @param path
+	 */
 	public static void saveBitmapFromURIToPath(Context context, Uri picURI, String path) {
 		saveBitmapToPath(context, bitmapFromURI(context, picURI), path);
 	}
 	
+	/**
+	 * uri에서 bitmap을 가져와서 path로 저장하되 포맷과 퀄리티를 추가로 설정
+	 * @param context
+	 * @param picURI
+	 * @param path
+	 * @param format
+	 * @param quality
+	 */
 	public static void saveBitmapFromURIToPath(Context context, Uri picURI,String path, Bitmap.CompressFormat format, int quality) {
 		saveBitmapToPath(context, bitmapFromURI(context, picURI), path, format, quality, true);
 	}
 	
+	/**
+	 * bitmap을 path에 저장. 포맷은 JPEG이고, 퀄리티는 80, internal = true
+	 * @param context
+	 * @param bitmap
+	 * @param path
+	 */
 	public static void saveBitmapToPath(Context context, Bitmap bitmap, String path) {
 		saveBitmapToPath(context, bitmap, path, Bitmap.CompressFormat.JPEG, 80, true);
 	}
 	
+	/**
+	 * bitmap을 path에 저장.
+	 * @param context 메인 컨텍스트
+	 * @param bitmap 비트맵 파일
+	 * @param path 경로
+	 * @param format 저장할 포맷
+	 * @param quality 압축 품질(0-100)
+	 * @param internal true이면 내부 버퍼에 저장, 아니면 내부 파일에 저장
+	 */
 	public static void saveBitmapToPath(Context context, Bitmap bitmap, String path, Bitmap.CompressFormat format, int quality, boolean internal) {
 		
 		
@@ -88,6 +140,12 @@ public class ImageManager {
         }
 	}
 	
+	/**
+	 * path에서 비트맵 로드 
+	 * @param path
+	 * @param internal
+	 * @return
+	 */
 	public static Bitmap loadBitmapFromPath(String path, boolean internal) {
 		if(internal) {
 			path = "data/data/kr.go.KNPA.Romeo/files/"+path;
@@ -104,6 +162,11 @@ public class ImageManager {
 		return bitmap;
 	}
 	
+	/**
+	 * path에서 비트맵 파일 삭제
+	 * @param path 파일 이름을 포함한 경로
+	 * @param internal
+	 */
 	public static void deleteBitmapFromPath(String path, boolean internal) {
 		try{
 			String parentPath = "";
