0cc634f7f3c502507f6d259dbec19290a8e02a18
Hosik Chae <pong0923@iMacDev.local>
2013-04-10 19:04:41 +0900

    Register, Enabled Check
    Register, Enabled Check

--- src/kr/go/KNPA/Romeo/IntroActivity.java
+++ src/kr/go/KNPA/Romeo/IntroActivity.java
@@ -9,14 +9,12 @@ import android.os.Bundle;
 
 
 public class IntroActivity extends BaseActivity{
-
 	private static IntroActivity _sharedActivity; 
 	private final int REQUEST_REGISTER_USER = 0;
 	private static StatusChecker checker; 
-	private int devStatus ;
-	private int userStatus;
+
 	private Bundle targetModuleInfo = null;
-	
+
 	public IntroActivity() {
 		super(R.string.changing_fragments);
 		_sharedActivity = this;	//?
@@ -24,6 +22,11 @@ public class IntroActivity extends BaseActivity{
 	public static IntroActivity sharedActivity() {
 		return _sharedActivity;
 	}
+
+	private boolean userRegistered = false;
+	private boolean userEnabled = false;
+	private boolean devRegistered = false;
+	private boolean devEnabled = false;
 	
 	@Override
 	protected void onNewIntent(Intent intent) {
@@ -31,51 +34,115 @@ public class IntroActivity extends BaseActivity{
 		long mil = 0;
 		if(_b!= null && _b.containsKey("TEST"))
 			mil = _b.getLong("TEST");
-		
+
 		targetModuleInfo = new Bundle();
 		Bundle b = intent.getExtras();
 		if(b != null) targetModuleInfo.putAll(b);
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		_sharedActivity = this;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.intro);
 
-		checker = new StatusChecker(this);
 		
+		Intent intent = getIntent();
+		targetModuleInfo = new Bundle();
+
+		Bundle b = intent.getExtras();
+		if(b != null) targetModuleInfo.putAll(b);
+		
+		checkRegistered();
+	}
+
+
+	private void checkRegistered () {
+		/**
+		 * 애플리케이션을 구동하기 위해 요구되는 상태를 체크하는 객체
+		 */
+		checker = new StatusChecker(this);
+
 		if ( !checker.isConnectedToNetwork() ) {
 			//TODO 인터넷이 안될 때 띄울 화면 처리.
 			//만약 처음에 킬 땐 안 됐다가 나중에 연결하는 걸 대비해서
 			//다음 사이트에 있는 내용을 참고하여 구현해야함
 			//http://shstarkr.tistory.com/158
 			finish();
-			return ;
 		}
 
-		
-		devStatus = checker.getDeviceStatus();
-		userStatus = checker.getUserStatus();
-		if ( devStatus  == StatusChecker.DEVICE_NOT_REGISTERED ) {
-			GCMRegisterManager.registerGCM(IntroActivity.this);
+		checkUserRegistered();
+
+		if(userRegistered) {
+			checkDeviceRegistered();
 			
-		}
-		
-		// DEVICE_REGISTERED_NOT_ENABLED or // DEVICE_REGISTERED_ENABLED
-		
-		if ( userStatus == StatusChecker.USER_NOT_REGISTERED ) {
-			startUserRegisterActivity();
-		} else {
-			
-			if ( isEnabled() ) {
-				runApplication();
+			if(devRegistered) {
+				goNextActivity();
 			} else {
-				startNotRegisteredActivity();
+				GCMRegisterManager.registerGCM(IntroActivity.this);
+				devRegistered = true;
+				goNextActivity();
 			}
-			
+				
+		} else {
+			startUserRegisterActivity();
+			// 기기 등록은 onResultActivity에서
 		}
+
 	}
+	
+	private void checkUserRegistered() {		
+		/**
+		 * 유저 상태 체크
+		 * USER_REGISTERED_ENABLED 상태가 아니라면 해당 상태마다
+		 * 필요한 Activity를 호출.
+		 * USER_REGISTERED_ENABLED일 경우 기기 인증 진행
+		 */
+		switch( checker.getUserStatus() ) {
+		
+			case StatusChecker.USER_NOT_REGISTERED:						//등록이 되어있지 않을 때
+				userRegistered = false;
+				userEnabled = false;
+	
+			case StatusChecker.USER_REGISTERED_NOT_ENABLED:				//아직 유저 활성화가 안됨
+				userRegistered = true;
+				userEnabled = false;
+				break;
+				
+			case StatusChecker.USER_REGISTERED_ENABLED :
+				userRegistered = true;
+				userEnabled = true;
+				break;
+		}
+
+	}
+
+	private void checkDeviceRegistered() {
+		/**
+		 * 기기 상태 체크
+		 * DEVICE_REGISTERED_ENABLED 상태가 아니라면 해당 상태마다
+		 * 필요한 Activity를 호출.
+		 * DEVICE_REGISTERED_ENABLED일 경우 기기 인증 진행
+		 */
+		switch( checker.getDeviceStatus() ) {
+		case StatusChecker.DEVICE_NOT_REGISTERED:
+			devRegistered = false;
+			devEnabled = false;
+			break;
+			
+		case StatusChecker.DEVICE_REGISTERED_NOT_ENABLED :
+			devRegistered = true;
+			devEnabled = false;
+			break;
+			
+		case StatusChecker.DEVICE_REGISTERED_ENABLED :
+			devRegistered = true;
+			devEnabled	= true;
+			break;
+		}
+
+	}
+
 
 	private void startUserRegisterActivity() {
 		Intent intent = new Intent(IntroActivity.this, UserRegisterActivity.class);
@@ -84,7 +151,7 @@ public class IntroActivity extends BaseActivity{
 		startActivityForResult(intent, REQUEST_REGISTER_USER);
 		overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
 	}
-	
+
 	private void startNotRegisteredActivity() {
 		Intent intent = new Intent(IntroActivity.this, NotRegisteredActivity.class);
 		startActivity(intent);
@@ -92,56 +159,62 @@ public class IntroActivity extends BaseActivity{
 		finish();
 	}
 
-	private boolean isEnabled() {
-		return userStatus == StatusChecker.USER_REGISTERED_ENABLED && devStatus == StatusChecker.DEVICE_REGISTERED_ENABLED;
+
+	private void goNextActivity() {
+		// userRegistered == true , devRegistered == true;
+		if(userEnabled && devEnabled)
+			runApplication();
+		else
+			startNotRegisteredActivity();
 	}
-	
 	private void runApplication() {
-		
+
 		/**
 		 * 모든 검증을 정상적으로 통과.
 		 * 메인 액티비티 시작 
 		 */
-		Intent moduleInfoIntent = getIntent();
-		Bundle moduleInfoBundle = null;
-		if(moduleInfoIntent != null)
-			moduleInfoBundle = moduleInfoIntent.getExtras();
 		
-		targetModuleInfo = new Bundle();
-		if(moduleInfoIntent != null) targetModuleInfo.putAll(moduleInfoBundle);
-		
-		
-		Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+		Intent intent = null;
+
+		intent = new Intent(IntroActivity.this, MainActivity.class);
 		intent.putExtras(targetModuleInfo);
-		
+
 		startActivity(intent);
 		finish();
 		overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-		
 	}
 
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if(resultCode == RESULT_OK) {
 			if(requestCode == REQUEST_REGISTER_USER) {
 				// UserRegisterActivity를 통해 User는 성공적으로 동록되었다고 본다. (UserRegisterActivity가 유저의 등록을 책임진다.)
+
+				
+				/////////////////////////
+				// userRegistered O, userEnabled ?(X)
+				////////////////////////
+				checkDeviceRegistered();
+				
+				if(devRegistered) {
+					goNextActivity();
+				} else {
+					GCMRegisterManager.registerGCM(IntroActivity.this);
+					goNextActivity();
+				}
+				
 				// TODO : 유저 등록, 디바이스 등록을 할만큼 처음 사용하는 것이라면,
 				// 메시지가 온적도 없을 것이고, 메시지가 와서 그것을 알리는 푸시 노티피케이션을 누르고
 				// 앱 && 액티비티가 실행되는 일도 없을 것이다.
 				// 따라서 이 곳에 해당 모듈이 실행되도록 하는 로직을 구성할 필요는 없다.
 				// TODO : 분실 신고, 분실 중 메시지 도착, 단말기 복구 ?? =>> 쌓여있던 메시지들은??,,,,
-				if ( isEnabled() ) {
-					runApplication();
-				} else {
-					startNotRegisteredActivity();
-				}
 			}
 		} else {
-			
+
 		}
 	}
-	
+
 	/*
 	public void removeIntroView(ViewGroup v) {
 		View view = (View)v.findViewById(R.id.intro);
@@ -149,14 +222,14 @@ public class IntroActivity extends BaseActivity{
 			view = (View)v.findViewWithTag("intro");
 		if(view != null)
 			v.removeView(view);
-		
+
 	}
-	*/
+*/
 	@Override
 	public void onBackPressed() {
 		//super.onBackPressed();
 		//h.removeCallbacks(logo);
 		return;
 	}
-	
+
 }
