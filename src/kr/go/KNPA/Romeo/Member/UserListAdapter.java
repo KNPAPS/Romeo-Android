86d29fffc83cbacc6760ea7c8b9a604b1954b61b
Young-Woo Choe <projet6634@gmail.com>
2013-04-16 16:45:14 +0900

    chat image

---
+++ src/kr/go/KNPA/Romeo/Member/UserListAdapter.java
@@ -0,0 +1,84 @@
+package kr.go.KNPA.Romeo.Member;
+
+import java.util.ArrayList;
+
+import kr.go.KNPA.Romeo.R;
+import kr.go.KNPA.Romeo.Config.Constants;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class UserListAdapter extends BaseAdapter {
+
+	private ArrayList<User> mArray;
+	private Context mContext;
+	
+	public UserListAdapter( Context context ) {
+		this.mContext = context;
+		this.mArray = new ArrayList<User>(); 
+	}
+	
+	public UserListAdapter( Context context, ArrayList<User> users ) {
+		this.mContext = context; 
+		this.mArray = users;
+	}
+	
+	@Override
+	public int getCount() {
+		return mArray.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		if ( position >= mArray.size() || position < 0 ) {
+			return mArray.get(position);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		if ( position>=mArray.size() || position <0 ) {
+			throw new IllegalStateException("couldn't access to index " + position);
+		}
+		
+		LayoutInflater inflater = LayoutInflater.from(mContext);
+
+		View v = null;
+		TextView departmentTV = null;
+		TextView nameTV = null;
+		TextView rankTV = null;
+		TextView roleTV = null;
+		if ( convertView == null ) {
+			v = inflater.inflate(R.layout.user_list_cell, parent, false);
+			
+		} else {
+			v = convertView;			
+		}
+		
+		departmentTV = (TextView)v.findViewById(R.id.department);
+		nameTV = (TextView)v.findViewById(R.id.name);
+		rankTV = (TextView)v.findViewById(R.id.rank);
+		roleTV = (TextView)v.findViewById(R.id.role);
+		
+		departmentTV.setText(mArray.get(position).department.nameFull);
+		nameTV.setText(mArray.get(position).name);
+		rankTV.setText(Constants.POLICE_RANK[mArray.get(position).rank]);
+		roleTV.setText(mArray.get(position).role);
+		
+		return v;
+	}
+
+
+}
