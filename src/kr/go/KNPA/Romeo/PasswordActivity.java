05250a8c816d0b43025a9b5925c5b4a75c43c565
Hosik Chae <pong0923@iMacDev.local>
2013-04-18 16:01:39 +0900

    new PasswordActivity
    new PasswordActivity

---
+++ src/kr/go/KNPA/Romeo/PasswordActivity.java
@@ -0,0 +1,14 @@
+package kr.go.KNPA.Romeo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class PasswordActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.password_activity);
+	}
+}
