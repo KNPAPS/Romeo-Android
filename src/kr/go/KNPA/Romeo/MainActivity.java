d2d68a82c167bf79d208d3ace86c16a17002ecf9
Young-Woo Choe <projet6634@gmail.com>
2013-05-27 16:08:12 +0900

    minor bugs

--- src/kr/go/KNPA/Romeo/MainActivity.java
+++ src/kr/go/KNPA/Romeo/MainActivity.java
@@ -1,4 +1,5 @@
 package kr.go.KNPA.Romeo;
+
 import kr.go.KNPA.Romeo.Base.Message;
 import kr.go.KNPA.Romeo.Chat.Room;
 import kr.go.KNPA.Romeo.Chat.RoomController;
@@ -23,204 +24,220 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
-
 public class MainActivity extends BaseActivity {
-	static MainActivity _sharedActivity = null;
-	public static final int MEMBER_SEARCH_ACTIVITY = 1;
-	
-	public boolean isRegistered = false;
-	
-	public MainActivity() {		// 생성자 
+	static MainActivity		_sharedActivity			= null;
+	public static final int	MEMBER_SEARCH_ACTIVITY	= 1;
+
+	public boolean			isRegistered			= false;
+
+	public MainActivity()
+	{ // 생성자
 		super(R.string.app_name);
 		_sharedActivity = this;
 	}
-	
-	public static MainActivity sharedActivity() {
+
+	public static MainActivity sharedActivity()
+	{
 		return _sharedActivity;
 	}
-	
+
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
-		
+
 		// customize the SlidingMenu
 		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
 
 		// set the Above View
-		setContentView(R.layout.content_frame);	// 레이아웃만 있는 빈 뷰
-				
-		// set the Behind View
-		setBehindContentView(R.layout.menu_frame); // 비하인드 프레임은, 메뉴 뷰다. 프레그먼트를 대입하기 위해 빈것으로 존재(베이스에서는)
+		setContentView(R.layout.content_frame); // 레이아웃만 있는 빈 뷰
 
-		Fragment fragment=null;
+		// set the Behind View
+		setBehindContentView(R.layout.menu_frame); // 비하인드 프레임은, 메뉴 뷰다. 프레그먼트를
+													// 대입하기 위해 빈것으로 존재(베이스에서는)
+
+		Fragment fragment = null;
 		// set the Above View
 		if (savedInstanceState != null)
 			fragment = getSupportFragmentManager().getFragment(savedInstanceState, "currentFragment"); // restore
 		if (fragment == null)
-			fragment = MemberFragment.memberFragment(MemberFragment.TYPE_MEMBERLIST);	// 첫화면										// 생성 		전혀 중요한 클래스가 아니다.
-		
+			fragment = MemberFragment.memberFragment(MemberFragment.TYPE_MEMBERLIST); // 첫화면
+																						// //
+																						// 생성
+																						// 전혀
+																						// 중요한
+																						// 클래스가
+																						// 아니다.
+
 		// ImageLoader 클래스 초기 설정
 		initImageLoader();
-		
-		getSupportFragmentManager()
-		.beginTransaction()
-		.replace(R.id.menu_frame, new MenuListFragment())
-		.commit();
+
+		getSupportFragmentManager().beginTransaction().replace(R.id.menu_frame, new MenuListFragment()).commit();
 
 		Intent intent = getIntent();
-		if(intent != null && intent.getExtras().containsKey(KEY.MESSAGE.TYPE)) {
-			try {
+		if (intent != null && intent.getExtras().containsKey(KEY.MESSAGE.TYPE))
+		{
+			try
+			{
 				Bundle b = intent.getExtras();
 				int type = b.getInt(KEY.MESSAGE.TYPE);
-				
+
 				int mainType = type / Message.MESSAGE_TYPE_DIVIDER;
 				int subType = type % Message.MESSAGE_TYPE_DIVIDER;
-				switch(mainType) {
-				case Message.MESSAGE_TYPE_CHAT :
+				switch (mainType)
+				{
+				case Message.MESSAGE_TYPE_CHAT:
 					Room room = new Room(b.getString(KEY.CHAT.ROOM_CODE));
 					goRoomFragment(subType, room);
 					break;
-				case Message.MESSAGE_TYPE_DOCUMENT :
+				case Message.MESSAGE_TYPE_DOCUMENT:
 					goDocumentFragment();
 					break;
-				case Message.MESSAGE_TYPE_SURVEY :
+				case Message.MESSAGE_TYPE_SURVEY:
 					goSurveyFragment();
 					break;
 				}
-				// currentFragment = new MemberFragment(MemberFragment.TYPE_MEMBERLIST);
-			} catch (Exception e) {
-				getSupportFragmentManager()
-				.beginTransaction()
-				.replace(R.id.content_frame, fragment)
-				.commit();												// 컨텐트 프레임과 현재(혹은 생성된) 프레그먼트를 바꾼다.
+				// currentFragment = new
+				// MemberFragment(MemberFragment.TYPE_MEMBERLIST);
 			}
-		} else {
-			getSupportFragmentManager()
-			.beginTransaction()
-			.replace(R.id.content_frame, fragment)
-			.commit();												// 컨텐트 프레임과 현재(혹은 생성된) 프레그먼트를 바꾼다.
+			catch (Exception e)
+			{
+				getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit(); // 컨텐트
+																												// 프레임과
+																												// 현재(혹은
+																												// 생성된)
+																												// 프레그먼트를
+																												// 바꾼다.
+			}
 		}
-				
+		else
+		{
+			getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment).commit();
+		}
+
 	}
-	
+
 	/**
 	 * ImageLoader class initialize
 	 */
-	public void initImageLoader() 
+	public void initImageLoader()
 	{
-		DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
-												.cacheInMemory()
-										        .cacheOnDisc()
-										        .build();
-												
-		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
-										        .defaultDisplayImageOptions(defaultOptions)
-										        .build();
-		ImageLoader.getInstance().init(config); // Do it on Application start		
+		DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().cacheInMemory().cacheOnDisc().build();
+
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext()).defaultDisplayImageOptions(defaultOptions).build();
+		ImageLoader.getInstance().init(config); // Do it on Application start
 	}
-	
-	public void goRoomFragment(int subType, Room room) 
+
+	public void goRoomFragment(int subType, Room room)
 	{
 		RoomListController roomListController = RoomListController.getInstance(subType);
 		RoomController roomController = new RoomController(room);
 		switchContent(roomListController);
 		pushContent(roomController);
 	}
-	
-	public void goDocumentFragment() {
+
+	public void goDocumentFragment()
+	{
 		DocumentFragment docFragment = DocumentFragment.documentFragment(Document.TYPE_RECEIVED);
 		switchContent(docFragment);
 	}
-	
-	public void goSurveyFragment() {
+
+	public void goSurveyFragment()
+	{
 		SurveyFragment survFragment = SurveyFragment.surveyFragment(Survey.TYPE_RECEIVED);
 		switchContent(survFragment);
 	}
-	
+
 	@Override
-	public void onSaveInstanceState(Bundle outState) {
-	}
-	
-	public void switchContent(Fragment fragment) 
+	public void onSaveInstanceState(Bundle outState)
 	{
-		getSupportFragmentManager() 
-			.beginTransaction()
-			.replace(R.id.content_frame, fragment, fragment.getClass().getSimpleName())
-			.commit();
+	}
+
+	public void switchContent(Fragment fragment)
+	{
+		getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, fragment, fragment.getClass().getSimpleName()).commit();
 		getSlidingMenu().showContent();
 	}
-	
-	public void pushContent(Fragment fragment) {
-		getSupportFragmentManager()				
-			.beginTransaction()
-			.setCustomAnimations(R.anim.slide_in_right, R.anim.stay, R.anim.stay, R.anim.slide_out_right)
-			.replace(R.id.content_frame, fragment, fragment.getClass().getSimpleName())
-			.addToBackStack(null)
-			.commit();
+
+	public void pushContent(Fragment fragment)
+	{
+		getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_right, R.anim.stay, R.anim.stay, R.anim.slide_out_right)
+				.replace(R.id.content_frame, fragment, fragment.getClass().getSimpleName()).addToBackStack(null).commit();
 	}
-	
-	public void popContent() {
+
+	public void popContent()
+	{
 		getSupportFragmentManager().popBackStack();
 	}
-		
+
 	@Override
-	public void toggle() {
+	public void toggle()
+	{
 		super.toggle();
-		
-		if(getSlidingMenu().isShown()) {
+
+		if (getSlidingMenu().isShown())
+		{
 			MenuListFragment.setMode(false);
-		} else {
+		}
+		else
+		{
 		}
 	}
-	 
-	@Override 
-	public boolean onKeyUp(int keyCode, KeyEvent event) {
+
+	@Override
+	public boolean onKeyUp(int keyCode, KeyEvent event)
+	{
 		FragmentManager fm = getSupportFragmentManager();
 		int count = fm.getBackStackEntryCount();
-		
-		if ( keyCode == KeyEvent.KEYCODE_MENU ) { 
+
+		if (keyCode == KeyEvent.KEYCODE_MENU)
+		{
 			toggle();
-			return true; 
-		} 
-		
-		if ( keyCode == KeyEvent.KEYCODE_BACK) {
-			 
-			if(count == 0) {
-				
-				if( getSlidingMenu().isMenuShowing() == true) {
+			return true;
+		}
+
+		if (keyCode == KeyEvent.KEYCODE_BACK)
+		{
+
+			if (count == 0)
+			{
+
+				if (getSlidingMenu().isMenuShowing() == true)
+				{
 					toggle();
 					return true;
 				}
-				
-				new AlertDialog.Builder(MainActivity.this)
-						.setIcon( this.getResources().getDrawable(kr.go.KNPA.Romeo.R.drawable.icon_dialog) )
-						.setTitle("다On")//context.getString(kr.go.KNPA.Romeo.R.string.)
-						.setMessage("종료하시겠습니까?")
-						.setPositiveButton(kr.go.KNPA.Romeo.R.string.ok, new DialogInterface.OnClickListener() {
+
+				new AlertDialog.Builder(MainActivity.this).setIcon(this.getResources().getDrawable(kr.go.KNPA.Romeo.R.drawable.icon_dialog)).setTitle("다On")// context.getString(kr.go.KNPA.Romeo.R.string.)
+						.setMessage("종료하시겠습니까?").setPositiveButton(kr.go.KNPA.Romeo.R.string.ok, new DialogInterface.OnClickListener() {
 							@Override
-							public void onClick(DialogInterface dialog, int which) {
+							public void onClick(DialogInterface dialog, int which)
+							{
 								dialog.dismiss();
 								android.os.Process.killProcess(android.os.Process.myPid());
 							}
 						}).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
 							@Override
-							public void onClick(DialogInterface dialog, int whichButton) {
-							    dialog.dismiss();
-							  }
-							})
-						.show();
+							public void onClick(DialogInterface dialog, int whichButton)
+							{
+								dialog.dismiss();
+							}
+						}).show();
 				return true;
-			} else {
+			}
+			else
+			{
 				this.popContent();
 				return true;
-			}	
+			}
 		}
-		
-		return super.onKeyDown(keyCode, event); 
-	} 
-	
+
+		return super.onKeyDown(keyCode, event);
+	}
+
 	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	public void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
 		super.onActivityResult(requestCode, resultCode, data);
 	}
 }
--- src/kr/go/KNPA/Romeo/Member/MemberManager.java
+++ src/kr/go/KNPA/Romeo/Member/MemberManager.java
@@ -1,99 +1,97 @@
 package kr.go.KNPA.Romeo.Member;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 
-import kr.go.KNPA.Romeo.Chat.Chatter;
 import kr.go.KNPA.Romeo.Config.Event;
 import kr.go.KNPA.Romeo.Config.KEY;
 import kr.go.KNPA.Romeo.Config.StatusCode;
 import kr.go.KNPA.Romeo.Connection.Connection;
 import kr.go.KNPA.Romeo.Connection.Data;
 import kr.go.KNPA.Romeo.Connection.Payload;
-import android.os.Handler;
-
 
 public class MemberManager {
-	public static int USER_IDX = 0;
-	public static int USER_NAME = 1;
-	public static int USER_DEPARTMENT = 2;
-	public static int USER_RANK = 3;
-	public static int USER_PIC = 4;
-	public static int USER_OBJECT = 5;
-	public static int USER_ALL = 6;
-	
-	private static MemberManager _instance = null;
-	
-	
-	private static HashMap<String, User> cachedUsers = null;	//! 캐시된 사람들 목록
-	private static HashMap<String, Department> cachedDepartment = null;
-	
+	public static int							USER_IDX			= 0;
+	public static int							USER_NAME			= 1;
+	public static int							USER_DEPARTMENT		= 2;
+	public static int							USER_RANK			= 3;
+	public static int							USER_PIC			= 4;
+	public static int							USER_OBJECT			= 5;
+	public static int							USER_ALL			= 6;
+
+	private static MemberManager				_instance			= null;
+
+	private static HashMap<String, User>		cachedUsers			= null; // !
+																			// 캐시된
+																			// 사람들
+																			// 목록
+	private static HashMap<String, Department>	cachedDepartment	= null;
+
 	// Apply Singleton Type
-	public static MemberManager sharedManager() {
-		if(_instance == null) {
+	public static MemberManager sharedManager()
+	{
+		if (_instance == null)
+		{
 			_instance = new MemberManager();
 		}
-		
-		if ( cachedUsers == null ) {
+
+		if (cachedUsers == null)
+		{
 			cachedUsers = new HashMap<String, User>();
 		}
-		
-		if ( cachedDepartment == null ) {
+
+		if (cachedDepartment == null)
+		{
 			cachedDepartment = new HashMap<String, Department>();
 		}
-		
+
 		return _instance;
 	}
-	
-	
+
 	/**
 	 * 다른 멤버의 정보 저장
-	 * @param member Member 객체
+	 * 
+	 * @param member
+	 *            Member 객체
 	 */
-	public void cacheUser(User user) {
-		if(cachedUsers == null)
+	public void cacheUser(User user)
+	{
+		if (cachedUsers == null)
 			cachedUsers = new HashMap<String, User>();
 		cachedUsers.put(user.idx, user);
 	}
-	
-	public void cacheDepartment(Department department) {
-		if(cachedDepartment == null)
+
+	public void cacheDepartment(Department department)
+	{
+		if (cachedDepartment == null)
 			cachedDepartment = new HashMap<String, Department>();
 		cachedDepartment.put(department.idx, department);
 	}
-	
-	
-	public User getUser(final String idx) {
+
+	public User getUser(final String idx)
+	{
 		User user = cachedUsers.get(idx);
-		if ( user != null ) {
+		if (user != null)
+		{
 			return user;
-		} else {
-			Thread t = new Thread(){
-				public void run() {
-					Payload request = new Payload().setEvent(Event.User.getUserInfo())
-							.setData( new Data().add(0, KEY.USER.IDX, idx) );
+		}
+		else
+		{
+			Thread t = new Thread() {
+				public void run()
+				{
+					Payload request = new Payload().setEvent(Event.User.getUserInfo()).setData(new Data().add(0, KEY.USER.IDX, idx));
 					Connection conn = new Connection().requestPayload(request).async(false).request();
 					Payload response = conn.getResponsePayload();
-					
-					if ( response.getStatusCode() == StatusCode.SUCCESS ) {
-						HashMap<String,Object> hm = response.getData().get(0);
-						
-						Department dep = new Department(
-													(String)hm.get(KEY.DEPT.IDX), 
-													(String)hm.get(KEY.DEPT.NAME), 
-													(String)hm.get(KEY.DEPT.FULL_NAME), 
-													null, 
-													Department.NOT_SPECIFIED
-												);
-						
-						User userFromServer = new User.Builder()
-											.idx((String)hm.get(KEY.USER.IDX))
-											.name((String)hm.get(KEY.USER.NAME))
-											.rank(Integer.parseInt((String)hm.get(KEY.USER.RANK)))
-											.role((String)hm.get(KEY.USER.ROLE))
-											.department(dep)
-											.build();
+
+					if (response.getStatusCode() == StatusCode.SUCCESS)
+					{
+						HashMap<String, Object> hm = response.getData().get(0);
+
+						Department dep = new Department((String) hm.get(KEY.DEPT.IDX), (String) hm.get(KEY.DEPT.NAME), (String) hm.get(KEY.DEPT.FULL_NAME), null, Department.NOT_SPECIFIED);
+
+						User userFromServer = new User.Builder().idx((String) hm.get(KEY.USER.IDX)).name((String) hm.get(KEY.USER.NAME)).rank(Integer.parseInt((String) hm.get(KEY.USER.RANK)))
+								.role((String) hm.get(KEY.USER.ROLE)).department(dep).build();
 
 						cacheUser(userFromServer);
 					}
@@ -101,264 +99,283 @@ public class MemberManager {
 			};
 
 			t.start();
-			try {
+			try
+			{
 				t.join();
-			} catch (InterruptedException e) {
+			}
+			catch (InterruptedException e)
+			{
 				e.printStackTrace();
 			}
 			User u = cachedUsers.get(idx);
+			if (u == null)
+			{
+				return cachedUsers.get("nulluseridx");
+			}
 			return u;
 		}
 	}
-	
-	public ArrayList<User> getUsers(ArrayList<String> idxs) {
-		
+
+	public ArrayList<User> getUsers(ArrayList<String> idxs)
+	{
+
 		ArrayList<User> users = new ArrayList<User>();
 		Data data = new Data();
-		for(int i=0; i< idxs.size(); i++) {
+		for (int i = 0; i < idxs.size(); i++)
+		{
 			User cachedUser = cachedUsers.get(idxs.get(i));
-			if ( cachedUser != null ) {
+			if (cachedUser != null)
+			{
 				users.add(cachedUser);
-			} else {
+			}
+			else
+			{
 				data.add(i, KEY.USER.IDX, idxs.get(i));
 			}
 		}
-		
-		//새로 서버에서 가져올 사람이 없으면 걍 리턴
-		if ( data.size() == 0 ) {
+
+		// 새로 서버에서 가져올 사람이 없으면 걍 리턴
+		if (data.size() == 0)
+		{
 			return users;
 		}
-		
-		//캐시되어있지 않은 사람들은 서버에서 정보를 가져옴
-		Payload request = new Payload().setEvent(Event.User.getUserInfo())
-								.setData( data );
-		
+
+		// 캐시되어있지 않은 사람들은 서버에서 정보를 가져옴
+		Payload request = new Payload().setEvent(Event.User.getUserInfo()).setData(data);
+
 		Connection conn = new Connection().requestPayload(request).async(false).request();
 		Payload response = conn.getResponsePayload();
-		
-		if ( response.getStatusCode() == StatusCode.SUCCESS ) {
+
+		if (response.getStatusCode() == StatusCode.SUCCESS)
+		{
 			Data responseData = response.getData();
-			
-			for(int i=0; i<responseData.size(); i++) {
-				HashMap<String,Object> hm = response.getData().get(i);
-				
-				Department dep = new Department(
-													(String)hm.get(KEY.DEPT.IDX), 
-													(String)hm.get(KEY.DEPT.NAME), 
-													(String)hm.get(KEY.DEPT.FULL_NAME), 
-													null, 
-													Long.parseLong(hm.get(KEY.DEPT.SEQUENCE)==null?"0":(String)hm.get(KEY.DEPT.SEQUENCE))
-												);
-				
-				User user = new User.Builder()
-									.idx((String)hm.get(KEY.USER.IDX))
-									.name((String)hm.get(KEY.USER.NAME))
-									.rank(Integer.parseInt((String)hm.get(KEY.USER.RANK)))
-									.role((String)hm.get(KEY.USER.ROLE))
-									.department(dep)
-									.build();
+
+			for (int i = 0; i < responseData.size(); i++)
+			{
+				HashMap<String, Object> hm = response.getData().get(i);
+
+				Department dep = new Department((String) hm.get(KEY.DEPT.IDX), (String) hm.get(KEY.DEPT.NAME), (String) hm.get(KEY.DEPT.FULL_NAME), null,
+						Long.parseLong(hm.get(KEY.DEPT.SEQUENCE) == null ? "0" : (String) hm.get(KEY.DEPT.SEQUENCE)));
+
+				User user = new User.Builder().idx((String) hm.get(KEY.USER.IDX)).name((String) hm.get(KEY.USER.NAME)).rank(Integer.parseInt((String) hm.get(KEY.USER.RANK)))
+						.role((String) hm.get(KEY.USER.ROLE)).department(dep).build();
 
 				cacheUser(user);
 				users.add(user);
 			}
-				
+
 			return users;
-		} else {
+		}
+		else
+		{
 			return new ArrayList<User>();
 		}
 	}
-	
+
 	/**
 	 * 해당 hash를 가진 사람에 대한 자료 cache에서 삭제
+	 * 
 	 * @param hash
 	 * @return
 	 */
-	public void removeCachedMember(String hash) {
+	public void removeCachedMember(String hash)
+	{
 		cachedUsers.remove(hash);
 	}
-	
-	
-	
+
 	/**
-	 * 부서 정보 가져와서 Department 객체에 담아 반환\n
-	 * parentHash까지는 가져온 데이터로부터 할당을 할 수 있지만\n
+	 * 부서 정보 가져와서 Department 객체에 담아 반환\n parentHash까지는 가져온 데이터로부터 할당을 할 수 있지만\n
 	 * Department 객체의 parentDept 멤버는 클라이언트쪽에서 따로 지정해줘야 함\n
+	 * 
 	 * @param deptHash
 	 * @return
 	 */
-	public Department getDeptartment(final String deptIdx) {
+	public Department getDeptartment(final String deptIdx)
+	{
 		Department department = cachedDepartment.get(deptIdx);
-		if ( department != null )
+		if (department != null)
 			return department;
-		
-		Thread t = new Thread(){
+
+		Thread t = new Thread() {
 			@Override
-			public void run() {
-				//setting request payload
+			public void run()
+			{
+				// setting request payload
 				Payload request = new Payload().setEvent(Event.User.getDepartmentInfo());
 				Data reqData = new Data();
-				reqData.add(0,KEY.DEPT.IDX,deptIdx);
-				
+				reqData.add(0, KEY.DEPT.IDX, deptIdx);
+
 				request.setData(reqData);
-				
+
 				Connection conn = new Connection().async(false).requestPayload(request).request();
 
 				Payload response = conn.getResponsePayload();
-				if ( response.getStatusCode() == StatusCode.SUCCESS ){
-					
+				if (response.getStatusCode() == StatusCode.SUCCESS)
+				{
+
 					Data respData = response.getData();
-					
-					Department dept = new Department(
-													(String)reqData.get(0, KEY.DEPT.IDX), 
-													(String)reqData.get(0, KEY.DEPT.NAME), 
-													(String)reqData.get(0, KEY.DEPT.FULL_NAME), 
-													(String)reqData.get(0, KEY.DEPT.PARENT_IDX), 
-													(String)respData.get(0, KEY.DEPT.SEQUENCE));
+
+					Department dept = new Department((String) reqData.get(0, KEY.DEPT.IDX), (String) reqData.get(0, KEY.DEPT.NAME), (String) reqData.get(0, KEY.DEPT.FULL_NAME), (String) reqData.get(
+							0, KEY.DEPT.PARENT_IDX), (String) respData.get(0, KEY.DEPT.SEQUENCE));
 					cacheDepartment(dept);
-				} else {
-					//TODO status 코드에 따라서 상황 처리	
+				}
+				else
+				{
+					// TODO status 코드에 따라서 상황 처리
 				}
 				super.run();
 			}
 		};
 
 		t.start();
-		
-		try {
+
+		try
+		{
 			t.join();
-		} catch (InterruptedException e) {
+		}
+		catch (InterruptedException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		Department d = cachedDepartment.get(deptIdx);
 		return d;
-		
+
 	}
-	
+
 	/**
 	 * 해당 부서의 하위 부서 목록을 가져온다
+	 * 
 	 * @param parentHash
 	 * @return
 	 */
-	private static ArrayList<Department> children;
-	public ArrayList<Department> getChildDepts(final String deptIdx) {
-		Thread t = new Thread(){
+	private static ArrayList<Department>	children;
+
+	public ArrayList<Department> getChildDepts(final String deptIdx)
+	{
+		Thread t = new Thread() {
 			@Override
-			public void run() {
-				//setting request payload
+			public void run()
+			{
+				// setting request payload
 				Payload request = new Payload().setEvent(Event.User.getChildDepartments());
-				
+
 				Data reqData = new Data();
-				
-				if(deptIdx == null || deptIdx.trim().length() < 1 || deptIdx.trim().equals("")) {
-					reqData.add(0,KEY.DEPT.IDX, "");
-				} else {
-					reqData.add(0,KEY.DEPT.IDX, deptIdx);
+
+				if (deptIdx == null || deptIdx.trim().length() < 1 || deptIdx.trim().equals(""))
+				{
+					reqData.add(0, KEY.DEPT.IDX, "");
 				}
-				
-				
+				else
+				{
+					reqData.add(0, KEY.DEPT.IDX, deptIdx);
+				}
+
 				request.setData(reqData);
-				
+
 				Connection conn = new Connection().requestPayload(request).async(false).request();
-				
+
 				Payload response = conn.getResponsePayload();
-				
-				if ( response.getStatusCode() == StatusCode.SUCCESS ){
+
+				if (response.getStatusCode() == StatusCode.SUCCESS)
+				{
 					Data respData = response.getData();
 					int nDeps = respData.size();
-					
-					//결과로 리턴할 객체
+
+					// 결과로 리턴할 객체
 					children = new ArrayList<Department>(nDeps);
-					
-					
-					for ( int i=0; i<nDeps; i++ ) {
-						Department dep = new Department(
-															(String)respData.get(i, KEY.DEPT.IDX), 
-															(String)respData.get(i, KEY.DEPT.NAME), 
-															(String)respData.get(i, KEY.DEPT.FULL_NAME), 
-															(String)respData.get(i, KEY.DEPT.PARENT_IDX), 
-															(String)respData.get(i, KEY.DEPT.SEQUENCE)
-														);
+
+					for (int i = 0; i < nDeps; i++)
+					{
+						Department dep = new Department((String) respData.get(i, KEY.DEPT.IDX), (String) respData.get(i, KEY.DEPT.NAME), (String) respData.get(i, KEY.DEPT.FULL_NAME),
+								(String) respData.get(i, KEY.DEPT.PARENT_IDX), (String) respData.get(i, KEY.DEPT.SEQUENCE));
 
 						cacheDepartment(dep);
-						
+
 						children.add(dep);
 					}
-					
-					
-				}else {
-					//TODO status 코드에 따라서 상황 처리
+
 				}
-				
+				else
+				{
+					// TODO status 코드에 따라서 상황 처리
+				}
+
 				super.run();
 			}
 		};
 
 		t.start();
-		try {
+		try
+		{
 			t.join();
-		} catch (InterruptedException e) {
+		}
+		catch (InterruptedException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return children;
 	}
-	
-	public ArrayList<User> getDeptMembers(String depIdx, boolean doRecursive) {
-		//setting request payload
+
+	public ArrayList<User> getDeptMembers(String depIdx, boolean doRecursive)
+	{
+		// setting request payload
 		Payload request = new Payload().setEvent(Event.User.getMembers());
-		
+
 		Data reqData = new Data();
-		
-		if( depIdx != null) {
+
+		if (depIdx != null)
+		{
 			reqData.add(0, KEY.DEPT.IDX, depIdx);
-			reqData.add(0, KEY.DEPT.FETCH_RECURSIVE, doRecursive==true?1:0 );
-		} else {
+			reqData.add(0, KEY.DEPT.FETCH_RECURSIVE, doRecursive == true ? 1 : 0);
+		}
+		else
+		{
 			// Root Department
 			reqData.add(0, KEY.DEPT.IDX, "");
-			reqData.add(0, KEY.DEPT.FETCH_RECURSIVE, doRecursive==true?1:0 );
+			reqData.add(0, KEY.DEPT.FETCH_RECURSIVE, doRecursive == true ? 1 : 0);
 		}
-		
+
 		request.setData(reqData);
-		
+
 		Connection conn = new Connection().async(false).requestPayload(request).request();
-		
+
 		Payload response = conn.getResponsePayload();
-		if ( response.getStatusCode() == StatusCode.SUCCESS ) {
+		if (response.getStatusCode() == StatusCode.SUCCESS)
+		{
 			Data respData = response.getData();
-			
+
 			int nUsers = respData.size();
 			ArrayList<User> members = new ArrayList<User>(nUsers);
-			
-			for (int i=0; i < nUsers; i++ ) {
+
+			for (int i = 0; i < nUsers; i++)
+			{
 				User user = null;
-				if(doRecursive == false) {
-					user = new User.Builder()
-										.idx( (String)respData.get(i,KEY.USER.IDX) )
-										.name( (String)respData.get(i,KEY.USER.NAME) )
-										.rank( Integer.parseInt( (String)respData.get(i,KEY.USER.RANK)) )
-										.role( (String)respData.get(i,KEY.USER.ROLE))
-										.department(getDeptartment(depIdx))
-										.build();
-				} else {
-					// recursive
-					user = new User.Builder()
-						.idx( (String)respData.get(i,KEY.USER.IDX) )
-						.name( (String)respData.get(i,KEY.USER.NAME) )
-						.rank( Integer.parseInt( (String)respData.get(i,KEY.USER.RANK)) )
-						.role( (String)respData.get(i,KEY.USER.ROLE))
-						//.department(dept) TODO
-						.build();
+				if (doRecursive == false)
+				{
+					user = new User.Builder().idx((String) respData.get(i, KEY.USER.IDX)).name((String) respData.get(i, KEY.USER.NAME)).rank(Integer.parseInt((String) respData.get(i, KEY.USER.RANK)))
+							.role((String) respData.get(i, KEY.USER.ROLE)).department(getDeptartment(depIdx)).build();
 				}
-				if(user != null)
+				else
+				{
+					// recursive
+					user = new User.Builder().idx((String) respData.get(i, KEY.USER.IDX)).name((String) respData.get(i, KEY.USER.NAME)).rank(Integer.parseInt((String) respData.get(i, KEY.USER.RANK)))
+							.role((String) respData.get(i, KEY.USER.ROLE))
+							// .department(dept) TODO
+							.build();
+				}
+				if (user != null)
 					members.add(user);
 			}
-			
+
 			return members;
-			
-		} else {
-			
+
+		}
+		else
+		{
+
 			return null;
 		}
 	}
