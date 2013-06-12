56d97d6e01ed02ee38157ce7bbc6d065fa8a6054
Young-Woo Choe <projet6634@gmail.com>
2013-06-12 15:39:51 +0900

    temporary auto update function

--- src/kr/go/KNPA/Romeo/Settings/SettingsFragment.java
+++ src/kr/go/KNPA/Romeo/Settings/SettingsFragment.java
@@ -1,20 +1,32 @@
 package kr.go.KNPA.Romeo.Settings;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import kr.go.KNPA.Romeo.MainActivity;
 import kr.go.KNPA.Romeo.R;
+import kr.go.KNPA.Romeo.Config.ConnectionConfig;
 import kr.go.KNPA.Romeo.Config.VibrationPattern;
 import kr.go.KNPA.Romeo.Util.RomeoDialog;
 import kr.go.KNPA.Romeo.Util.UserInfo;
+import kr.go.KNPA.Romeo.Util.WaiterView;
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.Cursor;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Vibrator;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -242,15 +254,18 @@ public class SettingsFragment extends Fragment {
 					}
 				});*/
 		
-		SettingsCellMaker.setTitle(cDevInfo, "최신 버전 다운로드");
+		SettingsCellMaker.setTitle(cDevInfo, "업데이트");
 		SettingsCellMaker.setOnClickListner(	cDevInfo, 
 				new OnClickListener() {
 					
 					@Override
 					public void onClick(View view) {
-						// TODO : 개발정보 준비
-						Toast.makeText(getActivity(), "준비중입니다.", Toast.LENGTH_SHORT).show();
+						
+						checkVersionAndDownload_Temporary();
+			
+						
 					}
+
 				});
 
 		return view;
@@ -273,5 +288,87 @@ public class SettingsFragment extends Fragment {
 		
 		if(lbb.getVisibility() == View.VISIBLE) lbb.setOnClickListener(lbbOnClickListener);
 		if(rbb.getVisibility() == View.VISIBLE) rbb.setOnClickListener(rbbOnClickListener);
+	}
+
+	private void checkVersionAndDownload_Temporary()
+	{
+		
+		PackageInfo pInfo = null;
+		try
+		{
+			pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+		}
+		catch (NameNotFoundException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		final String version = pInfo.versionName;
+		
+		WaiterView.showDialog(getActivity());
+		new Thread() {
+			@SuppressLint("InlinedApi")
+			public void run() 
+			{
+				try {
+					URL url = new URL(ConnectionConfig.SERVER_HOST+"juliette/version.txt");
+					
+					BufferedReader in = new BufferedReader(
+					            new InputStreamReader(
+					            url.openStream()));
+	
+					String lastestVersion = "";
+					String tmp;
+					while ((tmp = in.readLine()) != null)
+					    lastestVersion += tmp;
+	
+					in.close();
+					if (lastestVersion.equals(version))
+					{
+						getActivity().runOnUiThread(new Runnable(){
+							public void run() 
+							{
+								Toast.makeText(getActivity(), "이미 최신버전입니다.", Toast.LENGTH_SHORT).show();
+								WaiterView.dismissDialog(getActivity());
+							}
+						});
+					}
+					else
+					{
+						getActivity().runOnUiThread(new Runnable(){
+							public void run() 
+							{
+								WaiterView.dismissDialog(getActivity());
+								Toast.makeText(getActivity(), "다운로드를 시작합니다.\n완료가 되면 재설치해주세요.", Toast.LENGTH_SHORT).show();
+							}
+						});
+						
+						Uri uri = Uri.parse("http://vo.to/daon");
+
+						DownloadManager.Request request = new DownloadManager.Request(uri);
+						List<String> pathSegments = uri.getPathSegments();
+						request.setTitle("다온");
+						request.setDescription("다온");
+						if (Build.VERSION.SDK_INT >= 11)							
+						request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+						request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, pathSegments.get(pathSegments.size()-1));
+						Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).mkdirs();
+						((DownloadManager)getActivity().getSystemService(Context.DOWNLOAD_SERVICE)).enqueue(request);
+						 
+					}
+				}
+				catch (Exception e)
+				{
+					getActivity().runOnUiThread(new Runnable(){
+						public void run() 
+						{
+							WaiterView.dismissDialog(getActivity());
+							Toast.makeText(getActivity(), "에러가 발생했습니다.\n다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+						}
+					});
+				}
+			}
+		}.start();
+		
 	}
 }
