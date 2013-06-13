5c016065170609fdda4615d5495d0f94977cb23d
Hosik Chae <pong0923@iMacDev.local>
2013-06-13 15:51:10 +0900

    Menu Unchecked trial 1

--- src/kr/go/KNPA/Romeo/Menu/ExpandableMenuListAdapter.java
+++ src/kr/go/KNPA/Romeo/Menu/ExpandableMenuListAdapter.java
@@ -9,6 +9,7 @@ import kr.go.KNPA.Romeo.MainActivity;
 import kr.go.KNPA.Romeo.R;
 import kr.go.KNPA.Romeo.Chat.Chat;
 import kr.go.KNPA.Romeo.Chat.RoomListFragment;
+import kr.go.KNPA.Romeo.DB.DAO;
 import kr.go.KNPA.Romeo.Document.Document;
 import kr.go.KNPA.Romeo.Document.DocumentFragment;
 import kr.go.KNPA.Romeo.EBook.EBookFragment;
@@ -19,7 +20,9 @@ import kr.go.KNPA.Romeo.Settings.BugReportFragment;
 import kr.go.KNPA.Romeo.Settings.SettingsFragment;
 import kr.go.KNPA.Romeo.Survey.Survey;
 import kr.go.KNPA.Romeo.Survey.SurveyFragment;
+import kr.go.KNPA.Romeo.Util.WaiterView;
 import android.content.Context;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -34,7 +37,7 @@ import android.widget.TextView;
 class ExpandableMenuListAdapter extends SimpleExpandableListAdapter implements OnGroupClickListener, OnChildClickListener {
 
 	private Context context;
-	
+	Handler unCheckedHandler;
 	private ExpandableMenuListAdapter(
 			Context context,
 			List<? extends Map<String, ?>> groupData,
@@ -49,6 +52,7 @@ class ExpandableMenuListAdapter extends SimpleExpandableListAdapter implements O
 		super(context, groupData, expandedGroupLayout, collapsedGroupLayout, groupFrom,
 				groupTo, childData, childLayout, childFrom, childTo);
 		this.context = context;
+		unCheckedHandler = new Handler();
 	}
 
 	
@@ -75,7 +79,7 @@ class ExpandableMenuListAdapter extends SimpleExpandableListAdapter implements O
 						R.layout.menu_list_cell_section_unfolded, R.layout.menu_list_cell_section_folded, 
 						new String[] { "section" }, new int[] { R.id.title }, 
 						childrenData, R.layout.menu_list_cell_item, 
-						new String[] { "title", "code" }, new int[] { R.id.title, R.id.code });
+						new String[] { "title"}, new int[] { R.id.title});
 		return adapter;
 	}
 	
@@ -88,6 +92,48 @@ class ExpandableMenuListAdapter extends SimpleExpandableListAdapter implements O
 		iconView.setImageResource( item.iconImage() );
 		TextView titleView = (TextView)view.findViewById(R.id.title);
 		titleView.setText( getContext().getString( item.title() ) );
+		
+		final TextView nUncheckedTV = (TextView)view.findViewById(R.id.nUnchecked);
+		final String[] codes = item.code().split(":");
+		
+		if(
+				codes[0].equalsIgnoreCase("CHAT") || 
+				codes[0].equalsIgnoreCase("DOCUMENT") || 
+				codes[0].equalsIgnoreCase("SURVEY") ) {
+			
+			final WaiterView wv = new WaiterView(getContext());
+			nUncheckedTV.setVisibility(View.VISIBLE);
+			wv.substituteView(nUncheckedTV);
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					int unChecked = 0;
+					if(codes[0].equalsIgnoreCase("CHAT")) {
+						int nCommand = DAO.chat(getContext()).getNumUnchecked(Chat.TYPE_COMMAND);
+						int nMeeting = DAO.chat(getContext()).getNumUnchecked(Chat.TYPE_MEETING);
+						unChecked = nCommand + nMeeting;
+					} else if (codes[0].equalsIgnoreCase("DOCUMENT")) {
+						int nDocument = DAO.document(getContext()).getNumUnchecked();
+						unChecked = nDocument;
+					} else if (codes[0].equalsIgnoreCase("SURVEY")) {
+						int nSurvey = DAO.survey(getContext()).getNumUnchecked();
+						unChecked = nSurvey;
+					}
+
+					final int uc = unChecked;
+					unCheckedHandler.post(new Runnable() {
+						
+						@Override
+						public void run() {
+							nUncheckedTV.setText("" + uc);
+							wv.restoreView();
+						}
+					});
+					
+				}
+			}).start();
+		}
 		return view;
 	}
 	
@@ -99,6 +145,59 @@ class ExpandableMenuListAdapter extends SimpleExpandableListAdapter implements O
 		iconView.setImageResource( item.iconImage() );
 		TextView titleView = (TextView)view.findViewById(R.id.title);
 		titleView.setText( getContext().getString( item.title() ) );
+		
+		final TextView nUncheckedTV = (TextView)view.findViewById(R.id.nUnchecked);
+		final String[] codes = item.code().split(":");
+		
+		if(
+				codes[0].equalsIgnoreCase("CHAT") || 
+				codes[0].equalsIgnoreCase("DOCUMENT") || 
+				codes[0].equalsIgnoreCase("SURVEY") ) {
+			
+			final WaiterView wv = new WaiterView(getContext());
+			nUncheckedTV.setVisibility(View.VISIBLE);
+			wv.substituteView(nUncheckedTV);
+			new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					int unChecked = 0;
+					if(codes[0].equalsIgnoreCase("CHAT")) {
+						if(codes[1].equalsIgnoreCase("COMMAND")) {
+							unChecked = DAO.chat(getContext()).getNumUnchecked(Chat.TYPE_COMMAND);
+						} else if(codes[1].equalsIgnoreCase("MEETING")) {
+							unChecked = DAO.chat(getContext()).getNumUnchecked(Chat.TYPE_MEETING);
+						} else
+							return;
+						
+					} else if (codes[0].equalsIgnoreCase("DOCUMENT")) {
+						if(codes[1].equalsIgnoreCase("RECEIVED"))
+							unChecked = DAO.document(getContext()).getNumUnchecked();
+						else
+							return;
+						
+					} else if (codes[0].equalsIgnoreCase("SURVEY")) {
+						if(codes[1].equalsIgnoreCase("RECEIVED"))
+							unChecked = DAO.survey(getContext()).getNumUnchecked();
+						else
+							return;
+					}
+
+					final int uc = unChecked;
+					unCheckedHandler.post(new Runnable() {
+						
+						@Override
+						public void run() {
+							nUncheckedTV.setText("" + uc);
+							wv.restoreView();
+							
+						}
+					});
+					
+				}
+			}).start();
+		}
+
 		return view;
 	}
 	
